6737ac228de810bb0242277fb6c5ba5fd224ebda
Antony Holmes <antony.b.holmes@gmail.com>
2019-01-21 12:42:45 -0500

    Updates and bug fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='libtsne',
-    version='0.2.0',
+    version='0.3.0',
     author='Antony B Holmes',
     author_email='antony.b.holmes@gmail.com',
     description='A library for creating t-sne plots',
